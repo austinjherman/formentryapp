1faf97d9187eece19d2de845b252fe3dc7b08851
Austin Herman <aherman@sociusmarketing.com>
2019-04-18 23:31:53 -0400

    extend request to validate incoming form submissions

---
+++ app/Http/Request/FormEntryStore.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Request;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FormEntryStore extends Request
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'phone' => 'required|max:25',
+            'email' => 'required|email'
+        ];
+    }
+
+     /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages() {
+        return [
+            'first_name.required' => 'Please enter your first name.',
+            'last_name.required'  => 'Please enter your last name.',
+            'phone.required'      => 'Please enter your phone number.',
+            'email.required'      => 'Please enter your email address.',
+        ];
+    }
+
+    public function validateOrRespond() {
+        $validator = Validator::make($this->all(), $this->rules(), $this->messages());
+        if ($validator->fails()) {
+            throw new HttpResponseException(response()->json(['errors' => $validator->errors()->all()], 422));
+        }
+    }
+
+}
