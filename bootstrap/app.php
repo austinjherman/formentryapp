5448789a8de01367dcdf7bc616fc83c4300c4270
Austin Herman <aherman@sociusmarketing.com>
2019-05-05 18:48:50 -0400

    work in progress

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,9 +57,11 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+if (getenv('CORS')) {
+    $app->middleware([
+        App\Http\Middleware\CorsMiddleware::class
+    ]);
+}
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
